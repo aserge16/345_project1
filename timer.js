d7344026dba7fc9e71296adb746753590874192c
aserge16 <aleks.sergeev2@gmail.com>
2019-02-25 02:23:18 -0500

    alert raised

--- timer.js
+++ timer.js
@@ -1,6 +1,11 @@
 
 startday = new Date();
 clockStart = startday.getTime();
+
+function clickHandler(e) {
+	setTimeout(handler, 1000);
+  }
+
 function initStopwatch() {
 	var currentTime = new Date();
 	return((currentTime.getTime() - clockStart)/1000);
@@ -11,7 +16,12 @@ function clock() {
 	var iMins = Math.round((tSecs-30)/60);
 	var sSecs ="" + ((iSecs > 9) ? iSecs : "0" + iSecs);
     var sMins ="" + ((iMins > 9) ? iMins : "0" + iMins);
-    document.getElementById("timer").innerHTML = sMins+":"+sSecs;
-    //window.setTimeout('clock()',1000);
+	document.getElementById("timer").value = sMins+":"+sSecs;
+	alert(sMins+":"+sSecs);
 }
-			
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('timer').addEventListener('click', clickHandler);
+	clock();
+  });
