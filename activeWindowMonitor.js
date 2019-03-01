d4fee22a2c25855d78ab185fbede57e27740352a
lamdang98 <ltdang16@earlham.edu>
2019-03-01 17:31:17 -0500

    my bad. forgot to add

--- activeWindowMonitor.js
+++ activeWindowMonitor.js
@@ -1,12 +1,13 @@
-chrome.windows.onCreated.addListener(setUrl(window)) ;
+chrome.windows.onCreated.addListener(setUrl()) ;
 chrome.tabs.onUpdated.addListener(setUrl());
 setInterval(resetClock, 1000);
+
 
 var urls;
 var lastTimestamp;
 var lastURL;
 
-function setUrl(window) {
+function setUrl() {
     startday = new Date();
     clockStart = startday.getTime();
     chrome.tabs.query({'active': true, 'currentWindow': true},
@@ -35,28 +36,32 @@ function resetClock() {
 		urls = tabs[0].url;
         });
 	if (urls !== lastURL) {
-		startday = new Date();
-		clockStart = startday.getTime();
-		chrome.storage.local.set({"savedTime":clockStart.toString()}, function() {
-			console.log('Saved time is ' + clockStart.toString());
-		});
-		chrome.storage.local.set({"savedURL": urls}, function(){
-			console.log('Saved URL is ' + urls);
-		});
+		// startday = new Date();
+		// clockStart = startday.getTime();
+		// chrome.storage.local.set({"savedTime":clockStart.toString()}, function() {
+		// 	console.log('Saved time is ' + clockStart.toString());
+		// });
+		// chrome.storage.local.set({"savedURL": urls}, function(){
+		// 	console.log('Saved URL is ' + urls);
+		// });
+		setUrl()
 	} else {
 		var currentTime = new Date();
 		clockStart = parseInt(lastTimestamp);
 		var tSecs = Math.round((currentTime.getTime() - clockStart)/1000);
 		var iMins = Math.round((tSecs-30)/60);
 		if (iMins > 0) {
-			var answer = window.confirm("Page has been open longer than 30 minutes, would you like to quit now?");
+			var answer = window.confirm(urls +" has been open longer than 30 minutes, would you like to quit now?");
 			if (answer){
 				chrome.tabs.query({'active': true, 'currentWindow': true}, 
 				function(tabs){
 					chrome.tabs.remove(tabs[0].id);
 					});
 				setUrl();
-			}
+				} else {
+					setUrl();
+				}
+			
 		}
 	}
 }
