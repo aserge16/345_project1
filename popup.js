fe7120b804ea704557a6dff1018f9b73e9c5cb29
lamdang98 <ltdang16@earlham.edu>
2019-03-04 19:43:39 -0500

    timer

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     var submitButton = document.getElementById("timeSubmission");
-    submitButton.addEventListener('click', updateTime);
+    submitButton.addEventListener('click', function(){
+        var minute = document.getElementById('Timer').value;
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {time: minute, click : "True" }, function(response) {});  
+        });
+    });
 })
-function updateTime(){
-    var minute = document.getElementById('Timer').value;
-    sessionStorage.setItem("savedDelay", minute);
-    sessionStorage.setItem("clicked", "true");
-}
+// function updateTime(){
+//     var minute = document.getElementById('Timer').value;
+//     sessionStorage.setItem("savedDelay", minute);
+//     sessionStorage.setItem("clicked", "true");
+// }
