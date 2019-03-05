98ca04cbe3c5b9686bf15c637b7796a15947a5cf
lamdang98 <ltdang16@earlham.edu>
2019-03-04 23:26:28 -0500

    completed edition

--- popup.js
+++ popup.js
@@ -3,8 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){ //listen to when the e
     var submitButton = document.getElementById("timeSubmission"); 
     submitButton.addEventListener('click', function(){ //listen for push event in the submit button
         var minute = document.getElementById('Timer').value;
+        if (minute == '' || isNaN(minute)){ //check if user's input is valid
+        	alert("Please input the proper minute you want to spend");
+        } else {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {time: minute, click : "True" }, function(response) {}); //send message and data from extension box to event.js 
         });
+    }
     });
 })
